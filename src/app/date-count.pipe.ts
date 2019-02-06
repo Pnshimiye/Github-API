32a6e0d18e8b3612bf7a92757892acbe2e38c1de
Pnshimiye <pnshimiye@gmail.com>
2019-02-06 10:04:42 +0200

    remove the date pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,8 +8,18 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any, args?: any): any {
     let today:Date = new Date();
     let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(value-todayWithNoTime )
     
+
+ 
+    var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
+
+    var dateCounter = dateDifferenceSeconds 
+
+    if (dateCounter >= 1 && value > todayWithNoTime){
+        return dateCounter;
+    }else{
+        return 0;
+    }
   }
-
-
 }
