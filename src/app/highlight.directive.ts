e079e5e86050ffe338696102b4ea673bb99bbd40
Pnshimiye <pnshimiye@gmail.com>
2019-02-06 09:37:54 +0200

    change the directive gro line trhough to highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -7,12 +7,12 @@ export class HighlightDirective {
 
   constructor(private elem:ElementRef){}
 
-  @HostListener("click")onClick(){
-    this.textDeco("line-through")
+  @HostListener("click")oninput(){
+    this.textDeco("highlight")
   };
 
   private textDeco(action:string){
-    
+
     this.elem.nativeElement.style.textDecoration=action;
 
   }
